e6d5615c9a6928533a4da333156c675c5852ac0d
sandeepkumardeshetti <sandeepkumardeshetti@gmail.com>
2024-05-07 16:53:34 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: { NEXT_PUBLIC_API_URL: "http://localhost/wpicms/graphql"},
     images : {
         
         remotePatterns :[
